559f604a21d0337e86b5c8a5ad6a3a5314d36643
taku7269 <taku.h72@gmail.com>
2016-07-30 16:02:19 +0900

    描画部分完成版

--- js/GameConfig.js
+++ js/GameConfig.js
@@ -1,10 +1,11 @@
 function GameConfig(){
-  this.width = 400;
-  this.height = 400;
+  this.width = 1200;
+  this.height = 800;
   this.window = "game";
-  this.buffer = "buffer";
+  this.checkPoint = "checkPoint";
 }
 GameConfig.prototype.getWidth = function(){return this.width;};
 GameConfig.prototype.getHeight = function(){return this.height;};
 GameConfig.prototype.getWindow = function(){return this.window;};
+GameConfig.prototype.getCheckPoint = function(){return this.checkPoint;};
 GameConfig.prototype.getBuffer = function(){return this.buffer;};
--- js/app.js
+++ js/app.js
@@ -6,17 +6,19 @@ window.addEventListener("load", function() {
   
   var fields = new Array();
   // フィールドを登録
-  for(var fieldXIndex = 0; fieldXIndex < gameConfig.getWidth(); fieldXIndex += Field.getWidth()){
-    for(var fieldYIndex = 0; fieldYIndex < gameConfig.getHeight(); fieldYIndex += Field.getHeight()){
-      var field = new Field(new Point(fieldXIndex, fieldYIndex)).getImage();
-      fields.push(field);
-    }
-  }
+//   for(var fieldXIndex = 0; fieldXIndex < gameConfig.getWidth(); fieldXIndex += Field.getWidth()){
+//     for(var fieldYIndex = 0; fieldYIndex < gameConfig.getHeight(); fieldYIndex += Field.getHeight()){
+//       var field = new Field(new Point(fieldXIndex, fieldYIndex)).getImage();
+//       fields.push(field);
+//     }
+//   }
+  
+  var fields = new Array();
+  fields.push(new Field(new Point(0 ,0)).getImage());
 
   var checkPoints = new Array();
   for(var checkPointIndex = 0; checkPointIndex < 3; checkPointIndex++){
     checkPoints.push(new CheckPoint(new Point(parseInt(Math.random() * gameConfig.getWidth()), parseInt(Math.random() * gameConfig.getHeight()))).getImage());  
-
   }
 
   var playerX = 0, playerY = 0;
@@ -24,48 +26,55 @@ window.addEventListener("load", function() {
   
   var canvas = document.getElementById(gameConfig.getWindow());
   var context = canvas.getContext('2d');
-  drawEvent(context, gameConfig, fields, checkPoints, player.getImage());
+  drawEvent(context, gameConfig, fields, player.getImage());
+
+  var checkPointCanvas = document.getElementById(gameConfig.getCheckPoint());
+  var ctxCheckPoint = checkPointCanvas.getContext('2d');
+  drawEvent(ctxCheckPoint, gameConfig, checkPoints, player.getImage());
   
 });
 
-function drawEvent(context, config, fields, checkPoints, player){
-  for(var fi in fields){
-    fields[fi].getImage().addEventListener('load', function(){
-      for(var fj in fields){
-        context.drawImage(fields[fj].getImage(), fields[fj].getX(), fields[fj].getY());
-      }
-    }, false);
-  }
+function drawEvent(context, config, objects, player){
+//   for(var fi in fields){
+//     fields[fi].getImage().addEventListener('load', function(){
+//       for(var fj in fields){
+//         context.drawImage(fields[fj].getImage(), fields[fj].getX(), fields[fj].getY());
+//       }
+//     }, false);
+//   }
   
-  for(var cpi in checkPoints){
-    checkPoints[cpi].getImage().addEventListener('load', function() {
-      for(var cpj in checkPoints){
-        context.drawImage(checkPoints[cpj].getImage(), checkPoints[cpj].getX(), checkPoints[cpj].getY());
+//     field.getImage().addEventListener('load', function(){
+//       context.drawImage(field.getImage(), field.getX(), field.getY());
+//     }, false);
+
+  
+  for(var cpi in objects){
+    objects[cpi].getImage().addEventListener('load', function() {
+      for(var cpj in objects){
+        context.drawImage(objects[cpj].getImage(), objects[cpj].getX(), objects[cpj].getY());
       }
     }, false);
   }
 
-  var socket = { on: function(){} };
-  var connect = function(){
-    if(!socket.connected){
-      socket = io.connect('http://10.11.12.191:3001/');
-    } else {
-      socket.connect();
-    }
-    socket.on('point', function(data){
-      player.setX(data.x);
-      player.setY(data.y);
-      console.log(data);
+//   var socket = { on: function(){} };
+//   var connect = function(){
+//     if(!socket.connected){
+//       socket = io.connect('http://10.11.12.191:3001');
+//     } else {
+//       socket.connect();
+//     }
+//     socket.on('point', function(data){
+//       player.setX(data.x);
+//       player.setY(data.y);
+//       console.log(data);
       
-      player.getImage().addEventListener('load', function(){
-    context.drawImage(player.getImage(), player.getX(), player.getY());
-  }, false);
-    });
-
-
-  };
+//       player.getImage().addEventListener('load', function(){
+//         context.drawImage(player.getImage(), player.getX(), player.getY());
+//       }, false);
+//     });
+//   };
   
-  connect();
+//  connect();
 
   console.log("player (" + player.getX() + "," + player.getY() + ")");
   
